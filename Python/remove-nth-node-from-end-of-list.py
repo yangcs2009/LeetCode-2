5d3a059513ccadf3ad54a9ea8be528c949d1f444
yangchangsheng <yangchangsheng@bytedance.com>
2020-07-11 15:27:17 +0800

    add Python/remove-nth-node-from-end-of-list.py

--- Python/remove-nth-node-from-end-of-list.py
+++ Python/remove-nth-node-from-end-of-list.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Time:  O(n)
 # Space: O(1)
 #
@@ -13,6 +14,7 @@
 # Try to do this in one pass.
 #
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -24,7 +26,30 @@ class ListNode:
             return "Nil"
         else:
             return "{} -> {}".format(self.val, repr(self.next))
-        
+
+
+class Solution1(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        # 要有表头，防止只有一个元素被删找不到列表的情况
+        dummy = pre = cur = ListNode(-1)
+        dummy.next = head
+        while n > 0 and cur.next:
+            cur = cur.next
+            n -= 1
+        while cur.next:
+            pre, cur = pre.next, cur.next
+
+        tmp = pre.next
+        pre.next = tmp.next
+        del tmp
+        return dummy.next
+
+
 class Solution:
     # @return a ListNode
     def removeNthFromEnd(self, head, n):
@@ -37,16 +62,18 @@ class Solution:
             
         while fast.next:
             slow, fast = slow.next, fast.next
-            
+
         slow.next = slow.next.next
         
         return dummy.next
 
 if __name__ == "__main__":
+    # head = ListNode(1)
+    # head.next = ListNode(2)
+    # head.next.next = ListNode(3)
+    # head.next.next.next = ListNode(4)
+    # head.next.next.next.next = ListNode(5)
+
     head = ListNode(1)
-    head.next = ListNode(2)
-    head.next.next = ListNode(3)
-    head.next.next.next = ListNode(4)
-    head.next.next.next.next = ListNode(5)
     
-    print Solution().removeNthFromEnd(head, 2)
+    print Solution1().removeNthFromEnd(head, 1)
