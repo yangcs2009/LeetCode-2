71cd6b47b134b2401ccb50afb2cd0e8995ae428e
yangchangsheng <yangchangsheng@bytedance.com>
2020-07-15 21:57:49 +0800

    updte

--- Python/generate-parentheses.py
+++ Python/generate-parentheses.py
@@ -1,13 +1,20 @@
+# coding: utf-8
 # Time:  O(4^n / n^(3/2)) ~= Catalan numbers
 # Space: O(n)
 #
-# Given n pairs of parentheses, write a function to generate 
+# Given n pairs of parentheses, write a function to generate
 # all combinations of well-formed parentheses.
 # 
 # For example, given n = 3, a solution set is:
 # 
 # "((()))", "(()())", "(())()", "()(())", "()()()"
 #
+
+# 思考方式是采用DFS
+# 如果左括号还能括就不断的画左括号,直到左括号的数量等于n
+# 接下来考虑右括号,因为对于一对括号,右括号不能出现在左括号前,所以当右括号数量小于左括号时,加上右括号
+# 当左右括号数量都等于n时,得到解
+
 
 class Solution:
     # @param an integer
--- Python/merge-two-sorted-lists.py
+++ Python/merge-two-sorted-lists.py
@@ -35,6 +35,32 @@ class Solution(object):
         curr.next = l1 or l2
         return dummy.next
 
+class Solution1(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = cur = ListNode(-1)
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        while l1:
+            cur.next = l1
+            l1 = l1.next
+            cur = cur.next
+        while l2:
+            cur.next = l2
+            l2 = l2.next
+            cur = cur.next
+        return dummy.next
 
 if __name__ == "__main__":
     l1 = ListNode(0)
