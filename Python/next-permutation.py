51711dfa43f4fc6d0a67115cda7b7c76785b5641
yangchangsheng <yangchangsheng@bytedance.com>
2020-07-18 22:42:17 +0800

    update next permutation

--- Python/next-permutation.py
+++ Python/next-permutation.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Time:  O(n)
 # Space: O(1)
 #
@@ -12,6 +13,9 @@
 # 3,2,1 -> 1,2,3
 # 1,1,5 -> 1,5,1
 #
+
+# 思想： 其实是从右向左找到第一个数字不再递增的位置，然后从右边找到一个刚好大于当前位的数字即可
+# https://www.bookstack.cn/read/wind-liang-eetcode/bbf257c543619182.md
 
 class Solution:
     # @param {integer[]} nums
@@ -33,11 +37,35 @@ class Solution:
         num[k], num[l] = num[l], num[k]
         num[k + 1:] = num[:k:-1]
 
+
+class Solution1(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        if len(nums) <= 1:
+            return
+        k = -1
+        for i in xrange(len(nums)-1, 0, -1):
+            if nums[i-1] < nums[i]:
+                k = i-1
+                break
+        if k < 0:
+            for i in xrange(len(nums)/2):
+                nums[i], nums[len(nums)-1-i] = nums[len(nums)-1-i], nums[i]
+            return
+
+        for i in xrange(len(nums)-1, k, -1):
+            if nums[i] > nums[k]:
+                nums[i], nums[k] = nums[k], nums[i]
+                break
+
+        nums[k+1:] = nums[:k:-1]
+
+
 if __name__ == "__main__":
-    num = [1, 4, 3, 2]
-    Solution().nextPermutation(num)
-    print num
-    Solution().nextPermutation(num)
-    print num
-    Solution().nextPermutation(num)
+    num = [4, 3, 2, 1]
+    Solution1().nextPermutation(num)
     print num
