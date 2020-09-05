27f2b40ee3f01691967fee586411e6ee3e4411e0
yangchangsheng <yangchangsheng@bytedance.com>
2020-09-05 11:51:52 +0800

    update rotate image

--- Python/rotate-image.py
+++ Python/rotate-image.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Time:  O(n^2)
 # Space: O(1)
 #
@@ -9,6 +10,7 @@
 # Could you do this in-place?
 #
 
+
 # Time:  O(n^2)
 # Space: O(1)
 class Solution:
@@ -16,19 +18,20 @@ class Solution:
     # @return a list of lists of integers
     def rotate(self, matrix):
         n = len(matrix)
-        
+
         # anti-diagonal mirror
         for i in xrange(n):
             for j in xrange(n - i):
-                matrix[i][j], matrix[n-1-j][n-1-i] = matrix[n-1-j][n-1-i], matrix[i][j]
-        
+                matrix[i][j], matrix[n - 1 - j][n - 1 - i] = matrix[n - 1 - j][n - 1 - i], matrix[i][j]
+
         # horizontal mirror
         for i in xrange(n / 2):
             for j in xrange(n):
-                matrix[i][j], matrix[n-1-i][j] = matrix[n-1-i][j], matrix[i][j]
-                
+                matrix[i][j], matrix[n - 1 - i][j] = matrix[n - 1 - i][j], matrix[i][j]
+
         return matrix
-                
+
+
 # Time:  O(n^2)
 # Space: O(n^2)
 class Solution2:
@@ -36,7 +39,31 @@ class Solution2:
     # @return a list of lists of integers
     def rotate(self, matrix):
         return [list(reversed(x)) for x in zip(*matrix)]
-    
+
+
+class Solution1(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+
+        length = len(matrix)
+        # 以对角线为中心，交换数据
+        for i in xrange(length):
+            for j in xrange(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+        # 以竖中心线为中心，交换数据
+        for i in xrange(length):
+            for j in xrange(length / 2):
+                matrix[i][j], matrix[i][length - 1 - j] = matrix[i][length - 1 - j], matrix[i][j]
+        return matrix
+
+
 if __name__ == "__main__":
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    print Solution().rotate(matrix)
+    print matrix
+    print Solution1().rotate(matrix)
+
